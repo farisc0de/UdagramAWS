668e9affa9052e343fea9342aad387cea3b83dad
Faris AL-Otaibi <farisksa79@gmail.com>
2023-01-06 17:33:22 +0300

    fixed code

--- udagram/udagram-frontend/src/app/feed/services/feed.provider.service.ts
+++ udagram/udagram-frontend/src/app/feed/services/feed.provider.service.ts
@@ -1,11 +1,11 @@
-import { Injectable } from "@angular/core";
-import { FeedItem, feedItemMocks } from "../models/feed-item.model";
-import { BehaviorSubject } from "rxjs";
+import { Injectable } from '@angular/core';
+import { FeedItem, feedItemMocks } from '../models/feed-item.model';
+import { BehaviorSubject } from 'rxjs';
 
-import { ApiService } from "../../api/api.service";
+import { ApiService } from '../../api/api.service';
 
 @Injectable({
-  providedIn: "root",
+  providedIn: 'root',
 })
 export class FeedProviderService {
   currentFeed$: BehaviorSubject<FeedItem[]> = new BehaviorSubject<FeedItem[]>(
@@ -15,14 +15,14 @@ export class FeedProviderService {
   constructor(private api: ApiService) {}
 
   async getFeed(): Promise<BehaviorSubject<FeedItem[]>> {
-    const req = await this.api.get("/feed");
+    const req = await this.api.get('/feed');
     const items = <FeedItem[]>req.rows;
     this.currentFeed$.next(items);
     return Promise.resolve(this.currentFeed$);
   }
 
   async uploadFeedItem(caption: string, file: File): Promise<any> {
-    const res = await this.api.upload("/feed", file, {
+    const res = await this.api.upload('/feed', file, {
       caption: caption,
       url: file.name,
     });
